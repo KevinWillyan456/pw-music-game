65622bb5432449aceb085eb771beb3898cda592b
Kevin Souza <kevinwillyansouza@gmail.com>
2023-07-21 19:58:10 -0400

    feat: adicionado eventos de captura com o teclado

--- JS/index.js
+++ JS/index.js
@@ -1,28 +1,50 @@
+const trail1 = document.querySelector(".trail-1");
+const trail2 = document.querySelector(".trail-2");
+const trail3 = document.querySelector(".trail-3");
+const trail4 = document.querySelector(".trail-4");
+
+let target1 = document.querySelectorAll(".target-1");
+let target2 = document.querySelectorAll(".target-2");
+let target3 = document.querySelectorAll(".target-3");
+let target4 = document.querySelectorAll(".target-4");
+
+const containerTarget = document.querySelector(".main-container .container");
+
 function isElementOverlapping(element1, element2) {
   const rect1 = element1.getBoundingClientRect();
-  const rect2 = element2.getBoundingClientRect();
 
-  // Verificar a sobreposição horizontal
-  const horizontalOverlap =
-    rect1.left < rect2.right && rect1.right > rect2.left;
+  for (let i = 0; i < element2.length; i++) {
+    const rect2 = element2[i].getBoundingClientRect();
 
-  // Verificar a sobreposição vertical
-  const verticalOverlap = rect1.top < rect2.bottom && rect1.bottom > rect2.top;
+    // Verificar a sobreposição horizontal
+    const horizontalOverlap =
+      rect1.left < rect2.right && rect1.right > rect2.left;
 
-  // Retorna true se houver sobreposição tanto na horizontal quanto na vertical
-  return horizontalOverlap && verticalOverlap;
+    // Verificar a sobreposição vertical
+    const verticalOverlap =
+      rect1.top < rect2.bottom && rect1.bottom > rect2.top;
+
+    // Retorna true se houver sobreposição tanto na horizontal quanto na vertical
+    if (horizontalOverlap && verticalOverlap) {
+      return true;
+    }
+  }
+
+  // Se não houver sobreposição com nenhum dos elementos em element2, retornamos false
+  return false;
 }
 
-const elemento1 = document.querySelector(".trail-1"); // Substitua 'elemento1' pelo ID do primeiro elemento
-const elemento2 = document.querySelector(".target-1"); // Substitua 'elemento2' pelo ID do segundo elemento
-
-// setInterval(() => {
-//   if (isElementOverlapping(elemento1, elemento2)) {
-//     console.log("Os elementos estão sobrepostos!");
-//   } else {
-//     console.log("Os elementos não estão sobrepostos.");
-//   }
-// }, 200);
+setInterval(() => {
+  const ele = document.createElement('div')
+  ele.classList = "target target-4"
+  containerTarget.appendChild(ele);
+  elementosAnimados = document.querySelectorAll(".target");
+  target1 = document.querySelectorAll(".target-1");
+  target2 = document.querySelectorAll(".target-2");
+  target3 = document.querySelectorAll(".target-3");
+  target4 = document.querySelectorAll(".target-4");
+  animateElementDown(elementosAnimados, window.innerHeight);
+}, 500);
 
 function animateElementDown(element, distance) {
   for (let i = 0; i < element.length; i++) {
@@ -41,7 +63,7 @@ function animateElementDown(element, distance) {
 
       element[i].style.top = currentPosition + "px";
 
-      if (element[i].offsetTop == targetPosition) {
+      if (element[i].offsetTop >= targetPosition) {
         element[i].remove();
       }
 
@@ -55,5 +77,60 @@ function animateElementDown(element, distance) {
 }
 
 // Exemplo de uso:
-const elementosAnimados = document.querySelectorAll(".target");
+let elementosAnimados = document.querySelectorAll(".target");
 animateElementDown(elementosAnimados, window.innerHeight); // Desce 200 pixels
+
+// seu_script.js
+const teclasPermitidas = ["d", "f", "j", "k"];
+let teclaJaPressionada = {}; // Armazenar o estado de cada tecla
+
+document.addEventListener("keydown", function (event) {
+  const teclaPressionada = event.key.toLowerCase();
+
+  // Verifica se a tecla pressionada está na lista de teclas permitidas e se já não foi pressionada antes
+  if (
+    teclasPermitidas.includes(teclaPressionada) &&
+    !teclaJaPressionada[teclaPressionada]
+  ) {
+    console.log("Tecla pressionada: " + teclaPressionada);
+
+    // Coloque aqui o código que você deseja executar quando uma das teclas for pressionada.
+    if (teclaPressionada == "d") {
+      if (isElementOverlapping(trail1, target1)) {
+        alert("acertou");
+      } else {
+        console.log("Errou");
+      }
+    }
+    if (teclaPressionada == "f") {
+      if (isElementOverlapping(trail2, target2)) {
+        alert("acertou");
+      } else {
+        console.log("Errou");
+      }
+    }
+    if (teclaPressionada == "j") {
+      if (isElementOverlapping(trail3, target3)) {
+        alert("acertou");
+      } else {
+        console.log("Errou");
+      }
+    }
+    if (teclaPressionada == "k") {
+      if (isElementOverlapping(trail4, target4)) {
+        alert("acertou");
+      } else {
+        console.log("Errou");
+      }
+    }
+
+    teclaJaPressionada[teclaPressionada] = true; // Define a tecla como pressionada para evitar repetições
+  }
+});
+
+document.addEventListener("keyup", function (event) {
+  const teclaPressionada = event.key.toLowerCase();
+
+  // Quando a tecla é liberada, a marcação é removida, permitindo que ela seja pressionada novamente no futuro
+  teclaJaPressionada[teclaPressionada] = false;
+});
